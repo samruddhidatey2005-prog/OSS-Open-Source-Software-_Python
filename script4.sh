0f7cc2221b15a8661552a4396eed5027103f8d4c
samruddhidatey2005-prog <samruddhidatey2005@gmail.com>
2026-03-31 21:51:44 +0530

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 # Script 4: Log File Analyzer
-# Usage: ./script4.sh /var/log/bootstrap.log
+# Usage: ./script4_log_analyzer.sh /var/log/syslog error
+
 LOGFILE=$1
 KEYWORD=${2:-"error"}
 COUNT=0
 
-if [ -f "$LOGFILE" ]; then
-    while IFS= read -r LINE; do
-        if echo "$LINE" | grep -iq "$KEYWORD"; then
-            COUNT=$((COUNT + 1))
-        fi
-    done < "$LOGFILE"
-    echo "Keyword '$KEYWORD' found $COUNT times in $LOGFILE"
-else
-    echo "Error: File $LOGFILE not found."
+if [ ! -f "$LOGFILE" ]; then
+    echo "Error: File not found!"
     exit 1
 fi
+
+# Count occurrences
+while IFS= read -r LINE; do
+    if echo "$LINE" | grep -iq "$KEYWORD"; then
+        COUNT=$((COUNT + 1))
+    fi
+done < "$LOGFILE"
+
+echo "Keyword '$KEYWORD' found $COUNT times in $LOGFILE"
+
+# Show last 5 matches
+echo "Last 5 matching lines:"
+grep -i "$KEYWORD" "$LOGFILE" | tail -5
